d9ad08236fdc65878460ffc109a827102ffd1d45
Inhyoe <melon7256@naver.com>
2022-07-28 04:13:11 +0900

    Hashed

--- React/src/index.js
+++ React/src/index.js
@@ -7,9 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter>
-  <React.StrictMode>
+  
     <App />
-  </React.StrictMode>
+  
   </HashRouter>
 );
 
